61ce9febb2d829722fa1e55793025f63e0fad337
Faizul Md Nor <71113657+faizulmdnor@users.noreply.github.com>
2024-11-15 19:38:54 +0800

    SARIMAX vs. ARIMA

--- Python Script/sales_testing.py
+++ Python Script/sales_testing.py
@@ -37,16 +37,27 @@ def plot_sales_chart(df, title):
     )
 
 def arima_model(colname, forecast_period, df, d):
-    '''
 
+    '''
+    order=(0, 0, 1): This specifies the parameters of the ARIMA model:
+    AR (AutoRegressive): The first parameter (0) is the order of the autoregressive part. An AR(0) means that the model does not use any past values of the series for predictions. In other words, it does not include any lag terms (previous values in the series) in its calculation.
+    I (Integrated): The second parameter (0) represents the order of differencing required to make the series stationary. A value of 0 means no differencing is applied, implying that the series is already stationary (its statistical properties do not change over time).
+    MA (Moving Average): The third parameter (1) is the order of the moving average part. An MA(1) model uses the past residual errors (the differences between the predicted and actual values) for forecasting. This means the model will use the previous period's error to adjust future predictions.
     :param colname:
     :param forecast_period:
     :param df:
     :param d:
     :return:
     '''
+
+    """    
+    AutoRegressive = 1
+    Integrated = 0
+    MovingAverage = 0
+    """
+
     # Fit ARIMA model
-    arima = ARIMA(df[colname], order=(0, 1, 1))
+    arima = ARIMA(df[colname], order=(1, 0, 0))
     model_arima = arima.fit()
     model = model_arima
 
@@ -88,8 +99,15 @@ def sarimax_model(colname, forecast_period, df, d):
     :param d:
     :return:
     '''
+
+    """ 
+    AutoRegressive = 1
+    Integrated = 1
+    MovingAverage = 1
+    """
+
     # Fit SARIMAX model
-    model_sarima = SARIMAX(df[colname], order=(0, 1, 1), seasonal_order=(1, 1, 1, 12))
+    model_sarima = SARIMAX(df[colname], order=(1, 1, 1), seasonal_order=(1, 1, 1, 12))
     model_sarima_fitted = model_sarima.fit(disp=False)
     model = model_sarima_fitted
 
