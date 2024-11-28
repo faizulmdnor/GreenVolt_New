800868362bae0dfc5a6abc911537b6067a662ee3
Faizul Md Nor <71113657+faizulmdnor@users.noreply.github.com>
2024-11-28 14:28:33 +0800

    add documentataion for random_Sales_and_Prediction.py

--- Python Script/sales_testing.py
+++ Python Script/sales_testing.py
@@ -1,10 +1,29 @@
-
+import pandas as pd
 from matplotlib import pyplot as plt
-from GreenVold_Predicted import arima_predicted
+from GreenVolt_Predicted import arima_predicted
 from GreenVolt_db import greenvolt
+
 data = greenvolt.query_table('Monthly_Sales')
 groupby_YearMonth = data.groupby(by=['YearMonth']).agg({'totalSales': 'sum'}).round(2)
-df_PredictedSales_Monthly = arima_predicted.arima_model(forecast_period=6, df=groupby_YearMonth, colname='totalSales', period_name='Month')
-plt.plot(df_PredictedSales_Monthly['Month'], df_PredictedSales_Monthly['totalSales'], color='b', marker='s')
-plt.plot(df_PredictedSales_Monthly['Month'], df_PredictedSales_Monthly['Predicted_totalSales'], color='r', marker='s', linestyle='dotted')
+groupby_YearMonth.reset_index(inplace=True)
+groupby_YearMonth['date'] = pd.to_datetime(groupby_YearMonth['YearMonth'])
+
+groupby_YearMonth_arima = groupby_YearMonth
+groupby_YearMonth_sarimax = groupby_YearMonth
+
+df_PredictedSales_Monthly = arima_predicted.arima_model(forecast_period=6, df_arima=groupby_YearMonth_arima, colname='totalSales', period_name='Month')
+df_PredictedSales_Monthly['date'] = pd.to_datetime(df_PredictedSales_Monthly['date']).dt.strftime('%Y-%m')
+
+plt.figure(1)
+plt.plot(df_PredictedSales_Monthly['date'], df_PredictedSales_Monthly['totalSales'], color='b', marker='s', label='total sales')
+plt.plot(df_PredictedSales_Monthly['date'], df_PredictedSales_Monthly['Predicted_totalSales'], color='r', marker='s', linestyle='dotted', label='predicted total sales')
+
+
+df_PredictedSales_Monthly_sarimax = arima_predicted.sarimax_model(forecast_period=6, df_sarimax=groupby_YearMonth_sarimax, colname='totalSales', period_name='Month')
+df_PredictedSales_Monthly_sarimax['date'] = pd.to_datetime(df_PredictedSales_Monthly_sarimax['date']).dt.strftime('%Y-%m')
+
+plt.figure(2)
+plt.plot(df_PredictedSales_Monthly_sarimax['date'], df_PredictedSales_Monthly_sarimax['totalSales'], color='y', marker='s', label='total sales')
+plt.plot(df_PredictedSales_Monthly_sarimax['date'], df_PredictedSales_Monthly_sarimax['totalSales'], color='g', marker='s', label='predicted sales')
+
 plt.show()
